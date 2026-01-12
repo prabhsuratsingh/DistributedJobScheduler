3d91b4b4dd18265ea875110f6173bd57567d374a
prabhsuratsingh <prabhsurat17@gmail.com>
2026-01-12 15:45:41 +0530

    setup docker

--- app/main.py
+++ app/main.py
@@ -1,6 +1,19 @@
-def main():
-    print("Hello from distributedjobscheduler!")
+from fastapi import FastAPI
 
+app = FastAPI()
+
+@app.get("/health")
+async def health():
+    return {"Status": "healthy"}
+
+@app.get("/health/redis")
+async def health_redis():
+    pass
+
+@app.get("/health/postgres")
+async def health_postgres():
+    pass
 
 if __name__ == "__main__":
-    main()
+    import uvicorn
+    uvicorn.run(app, host="0.0.0.0", port=8080)
