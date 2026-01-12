f2896c29aa3f8f926e235184e3d74db3bd6a9d34
prabhsuratsingh <prabhsurat17@gmail.com>
2026-01-12 16:34:42 +0530

    setup postgres database

---
+++ app/database/db.py
@@ -0,0 +1,18 @@
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+
+from app.core.config import DATABASE_URL
+from app.database.base import Base
+
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+def init_db():
+    Base.metadata.create_all(bind=engine)
